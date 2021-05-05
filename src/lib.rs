2c7a93b2a1697b2b3050289627af6116a0aeb3a4
Jonas Platte <jplatte@users.noreply.github.com>
2021-05-05 10:15:11 +0200

    Improve formatting of crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,13 @@
 )]
 //! A crate for generating large prime numbers, suitable for cryptography.
 //!
-//! `Primes` are generated similarly to OpenSSL except it applies some recommendations from the (Prime and Prejudice)[https://eprint.iacr.org/2018/749.pdf]
+//! Primes are generated similarly to OpenSSL except it applies some recommendations
+//! from the [Prime and Prejudice](https://eprint.iacr.org/2018/749.pdf).
 //!
 //! 1. Generate a random odd number of a given bit-length.
 //! 2. Divide the candidate by the first 2048 prime numbers
 //! 3. Test the candidate with Fermat's Theorem.
-//! 4. Runs Baillie-PSW test with log2(bits) + 5 Miller-Rabin tests
+//! 4. Runs Baillie-PSW test with `log2(bits) + 5` Miller-Rabin tests
 
 mod common;
 pub mod error;
